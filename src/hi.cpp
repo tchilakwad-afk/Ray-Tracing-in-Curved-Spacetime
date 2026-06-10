d210f3a3f86276a37873f0c234e493d0a2c9cc93
Tanush <tchilakwad@gmail.com>
2026-06-10 14:36:36 +0000

    textures

--- src/hi.cpp
+++ src/hi.cpp
@@ -2,20 +2,36 @@
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 800, 600 } ), "Hello world!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Blue );
+	// sf::RenderWindow window( sf::VideoMode( { 800, 600 } ), "Hello world!" );
+	// sf::CircleShape shape( 100.f );
+	// shape.setFillColor( sf::Color::Blue );
 
-	while ( window.isOpen() )
-	{
-		while ( const std::optional event = window.pollEvent() )
-		{
-			if ( event->is<sf::Event::Closed>() )
-				window.close();
-		}
+	// while ( window.isOpen() )
+	// {
+	// 	while ( const std::optional event = window.pollEvent() )
+	// 	{
+	// 		if ( event->is<sf::Event::Closed>() )
+	// 			window.close();
+	// 	}
 
-		window.clear();
-		window.draw( shape );
-		window.display();
-	}
+	// 	window.clear();
+	// 	window.draw( shape );
+	// 	window.display();
+	// }
+	sf ::RenderWindow window( sf::VideoMode( { 800, 600 } ), "My window" );
+	sf ::RenderTexture renderTexture({500, 500});
+	sf ::CircleShape shape(100.f);
+	shape.setFillColor(sf::Color::Magenta);
+
+	renderTexture.clear();
+	renderTexture.draw(shape);
+	renderTexture.display();
+
+	const sf::Texture& texture = renderTexture.getTexture();
+
+	sf::Sprite sprite(texture);
+	window.clear();
+	window.draw(sprite);
+	window.display();
+
 }
