00a196bdef89b70491343db09d38a9b966b8e197
Tanush <tchilakwad@gmail.com>
2026-06-10 16:52:32 +0000

    lines

--- src/hi.cpp
+++ src/hi.cpp
@@ -3,15 +3,9 @@
 int main()
 {
 	sf ::RenderWindow window(sf::VideoMode({800, 600}), "Window");
-	sf ::ConvexShape convex;
+	sf::RectangleShape line({150.f, 5.f});
 
-	convex.setPointCount(5);
-	convex.setPoint(0, {50.f, 50.f});
-	convex.setPoint(1, {200.f, 60.f});
-	convex.setPoint(2, {170.f, 140.f});
-	convex.setPoint(3, {80.f, 150.f});
-	convex.setPoint(4, {50.f, 100.f});
-	convex.setFillColor(sf::Color(100, 12, 100));
+	line.rotate(sf:: degrees(45));
 
 	while(window.isOpen())
 	{
@@ -22,7 +16,7 @@ int main()
 	 	}
 
  	window.clear();
- 	window.draw(convex);
+ 	window.draw(line);
  	window.display();
 	}
 }
