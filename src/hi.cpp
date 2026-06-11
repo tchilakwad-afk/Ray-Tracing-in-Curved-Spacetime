75c1bb4314cb17ba97ee2eabd69b8e1911fff37e
Tanush <tchilakwad@gmail.com>
2026-06-11 08:06:46 +0000

    back to rectangle

--- src/hi.cpp
+++ src/hi.cpp
@@ -2,45 +2,11 @@
 using namespace std;
 using namespace sf;
 
-class EllipseShape : public Shape
-{
-	public :
-	explicit EllipseShape(Vector2f radius = {0, 0}) : m_radius(radius)
-	{
-		update();
-	}
-	void setRadius(Vector2f radius)
-	{
-		m_radius = radius;
-		update();
-	}
-	Vector2f getRadius() const
-	{
-		return m_radius;
-	}
-	size_t getPointCount() const override
-	{
-		return 30;
-	}
-	Vector2f getPoint(size_t index) const override
-	{
-		static constexpr float pi = 3.141592654f;
-
-		float angle = index * 2 * pi / getPointCount() - pi / 2;
-		float x = cos(angle) * m_radius.x;
-		float y = sin(angle) * m_radius.y;
-
-		return m_radius + Vector2f(x, y);
-	}
-	private:
-	Vector2f m_radius;
-};
-
 int main()
 {
 	RenderWindow window(VideoMode({800, 600}), "Window");
-	EllipseShape e({200.f, 150.f});
-	e.setFillColor(Color::Cyan);
+	RectangleShape rect({150.f, 200.f});
+	rect.setFillColor(Color::Magenta);
 
 	while(window.isOpen())
 	{
@@ -51,7 +17,7 @@ int main()
 	 	}
 
  	window.clear();
- 	window.draw(e);
+ 	window.draw(rect);
  	window.display();
 	}
 }
