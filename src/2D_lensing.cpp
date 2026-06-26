0d5af7fda18f453d55c3a7641d6798f88ef25263
Tanush <tchilakwad@gmail.com>
2026-06-26 17:05:32 +0000

    2dlensinginclude

--- src/2D_lensing.cpp
+++ src/2D_lensing.cpp
@@ -1,83 +1,47 @@
 #include "2D_lensing.hpp"
 
-// #include <SFML/Graphics.hpp>
-// #include<vector>
-// #include<cmath>
+BlackHole::BlackHole(Vector2f pos, float m): position(pos), mass(m){
+	r_s = 2.0 * G * mass / (c*c);
+}
 
-// using namespace std;
-// using namespace sf;
+void BlackHole::draw(RenderWindow& window){
+	CircleShape circle(r_s / scale);
+	circle.setFillColor(Color::Red);
+	circle.setPosition(position - Vector2f(r_s/scale, r_s/scale));
+	
+	window.draw(circle);
+}
 
-// #define PI 3.14159265358979323846
+void Ray::update(float dlambda, BlackHole SagA){
+	if(!active) return;
 
-// double c = 299792458.0;
-// double G = 6.67430e-11;
-// double scale = 1e8;
-// double c_pixels = c / scale;
+	runge_kutta4(dlambda, *this, SagA);
 
-struct Ray;
-struct BlackHole;
-void geodesic(double& d2t, double& d2r, double& d2phi, Ray ray, BlackHole SagA);
-void runge_kutta4(double dlambda, Ray& ray, BlackHole SagA);
+	position.x = r*cos(phi) / scale + 400.f;
+	position.y = r*sin(phi) / scale + 300.f;
 
-struct BlackHole{
-	Vector2f position;
-	double mass;
-	double r_s;
+	trail.push_back(Vertex(position, Color(225, 255, 0)));
 
-	BlackHole(Vector2f pos, float m) : position(pos), mass(m){
-		r_s = 2.0 * G * mass / (c*c);
-	}
+	if(position.x < 0 || position.x > 800 || position.y < 0 || position.y > 600)
+		active = false;
+	if(r < SagA.r_s)
+		active = false;
+}
 
-	void draw(RenderWindow& window){
-		CircleShape circle(r_s / scale);
-		circle.setFillColor(Color::Red);
-		circle.setPosition(position - Vector2f(r_s/scale, r_s/scale));
-		
-		window.draw(circle);
-	}
-};
+Ray::Ray(Vector2f pos, Vector2f vel, BlackHole SagA): position(pos), velocity(vel), active(true){
+	this->r = sqrt((position.x-400.f)*(position.x-400.f) + (position.y-300.f)*(position.y-300.f))*scale;
+	this->phi = atan2(position.y-300.f, position.x-400.f);
+	t = 0.0;
 
-struct Ray{
-	Vector2f position;
-	Vector2f velocity;
-	vector<Vertex> trail;
-	float currentTime = 0.0f;
-	bool active;
+	dr = (cos(phi)*velocity.x + sin(phi)*velocity.y)*scale;
+	dphi = (cos(phi)*velocity.y - sin(phi)*velocity.x)*scale/r;
+	dt = sqrt((dr*dr/(1-SagA.r_s/r) + r*r*dphi*dphi)/(1-SagA.r_s/r))/c;
+}
 
-	double r; double phi; double t;
-	double dr; double dphi; double dt;
-
-	Ray(Vector2f pos, Vector2f vel, BlackHole SagA): position(pos), velocity(vel), active(true){
-		this->r = sqrt((position.x-400.f)*(position.x-400.f) + (position.y-300.f)*(position.y-300.f))*scale;
-		this->phi = atan2(position.y-300.f, position.x-400.f);
-		t = 0.0;
-
-		dr = (cos(phi)*velocity.x + sin(phi)*velocity.y)*scale;
-		dphi = (cos(phi)*velocity.y - sin(phi)*velocity.x)*scale/r;
-		dt = sqrt((dr*dr/(1-SagA.r_s/r) + r*r*dphi*dphi)/(1-SagA.r_s/r))/c;
-	}
-
-	void update(float dlambda, BlackHole SagA){
-		if(!active) return;
-
-		runge_kutta4(dlambda, *this, SagA);
-
-		position.x = r*cos(phi) / scale + 400.f;
-		position.y = r*sin(phi) / scale + 300.f;
-
-		trail.push_back(Vertex(position, Color(225, 255, 0)));
-
-		if(position.x < 0 || position.x > 800 || position.y < 0 || position.y > 600)
-			active = false;
-		if(r < SagA.r_s)
-			active = false;
-	}
-
-	void draw(RenderWindow& window){
-		if(trail.size() >= 2)
-			window.draw(trail.data(), trail.size(), PrimitiveType::LineStrip);
-	}
-};
+void Ray::draw(RenderWindow& window){
+	if(trail.size() >= 2)
+		window.draw(trail.data(), trail.size(), PrimitiveType::LineStrip);
+}
 
 void geodesic(double& d2t, double& d2r, double& d2phi, Ray ray, BlackHole SagA){
 	double cs_t[2]; double cs_r[3]; double cs_phi[2];
